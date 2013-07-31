b2bcaa337320817bde8ec06f6e84f4ad44625c29
bosstep <bosstep@comcast.net>
2013-07-31 15:21:01 -0600

    Create ui.R

---
+++ ui.R
@@ -0,0 +1,17 @@
+library(shiny)
+
+# 001 class example ui.R
+# define UI for miles per gallon application
+
+shinyUI(pageWithSidebar(
+
+# Application title
+
+  headerPanel("Miles Per Gallon"), 
+
+  sidebarPanel(), 
+
+  mainPanel()
+
+))
+  
