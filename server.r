096696f79af7f5d2992f330cbbf8f1821c3a192c
bosstep <bosstep@comcast.net>
2013-07-31 15:21:45 -0600

    Create server.r

---
+++ server.r
@@ -0,0 +1,10 @@
+library(shiny)
+library(datasets)
+
+# class example 001 server.R
+
+# define server logic to plot various variables against mpg
+# empty now, but server.R is always required
+
+shinyServer(function(input, output) {
+})
