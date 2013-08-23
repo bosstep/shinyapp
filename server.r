1146a0cbbb5442c733a4ee95bd308fec1acb0e25
bosstep <bosstep@comcast.net>
2013-08-23 10:48:56 -0600

    Update server.r

--- server.r
+++ server.r
@@ -1,10 +1,50 @@
 library(shiny)
 library(datasets)
 
-# class example 001 server.R
-
-# define server logic to plot various variables against mpg
-# empty now, but server.R is always required
-
+# Define server logic required to summarize and view the selected dataset
 shinyServer(function(input, output) {
+
+  # By declaring datasetInput as a reactive expression we ensure that:
+  #
+  #  1) It is only called when the inputs it depends on changes
+  #  2) The computation and result are shared by all the callers (it 
+  #     only executes a single time)
+  #  3) When the inputs change and the expression is re-executed, the
+  #     new result is compared to the previous result; if the two are
+  #     identical, then the callers are not notified
+  #
+  datasetInput <- reactive({
+    switch(input$dataset,
+           "Accounting Department" = salaryfakeA,
+           "Human Resources Department" = salaryfakeB)           
+  })
+
+  # The output$caption is computed based on a reactive expression that
+  # returns input$caption. When the user changes the "caption" field:
+  #
+  #  1) This expression is automatically called to recompute the output 
+  #  2) The new caption is pushed back to the browser for re-display
+  # 
+  # Note that because the data-oriented reactive expression below don't 
+  # depend on input$caption, those expression are NOT called when 
+  # input$caption changes.
+  output$caption <- renderText({
+    input$caption
+  })
+
+  # The output$summary depends on the datasetInput reactive expression, 
+  # so will be re-executed whenever datasetInput is re-executed 
+  # (i.e. whenever the input$dataset changes)
+  output$summary <- renderPrint({
+    dataset <- datasetInput()
+    model <- lm(salary ~ age + band, data=dataset)
+    summary(model)
+  })
+
+  # The output$view depends on both the databaseInput reactive expression
+  # and input$obs, so will be re-executed whenever input$dataset or 
+  # input$obs is changed. 
+  output$view <- renderTable({
+    head(datasetInput(), n = input$obs)
+  })
 })
